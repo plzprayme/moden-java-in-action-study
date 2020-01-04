3c9872fc759e796388b646f45b3efa31589f65d8
plzprayme <dbfpzk142@gmail.com>
2020-01-04 12:12:15 +0900

    9장: 전략 패턴 람다로 바꾸기

--- src/designpattern/Strategy/Strategy.java
+++ src/designpattern/Strategy/Strategy.java
@@ -6,9 +6,13 @@ public class Strategy {
     private final ValidationStrategy strategy;
 
     public static void strategyRun() {
-        Strategy numericValidator = new Strategy(new IsNumeric());
+//        Strategy numericValidator = new Strategy(new IsNumeric());
+        Strategy numericValidator =
+                new Strategy((String s) -> s.matches("\\d+"));
         boolean b1 = numericValidator.validate("aaa");
-        Strategy lowerCaseValidator = new Strategy(new IsAllLowerCase());
+//        Strategy lowerCaseValidator = new Strategy(new IsAllLowerCase());
+        Strategy lowerCaseValidator =
+                new Strategy((String s) -> s.matches("[a-z]+"));
         boolean b2 = lowerCaseValidator.validate(("bbbb"));
 
         System.out.println(b1 + " / " + b2);
